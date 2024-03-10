cf13537db00de436d0b130c2af5a4636ce6e2bf7
louise <a115744446@gmail.com>
2024-03-11 05:28:21 +0800

    Change datatype of bandwidth.

--- Bandwidth.sql
+++ Bandwidth.sql
@@ -3,6 +3,6 @@ CREATE TABLE Bandwidth(
         ipv4_addr NVARCHAR(40), -- IPV4 address
         day DATETIME DEFAULT (STRFTIME('%Y-%m-%d', CURRENT_TIMESTAMP, 'localtime')),
         hms DATETIME DEFAULT (STRFTIME('%H:%M:%S', CURRENT_TIMESTAMP, 'localtime')),
-        measured_bandwidth REAL, -- 測量結果
+        measured_bandwidth INT, -- 測量結果, Mbits/sec
         FOREIGN KEY(ipv4_addr) REFERENCES Device (ipv4_addr)
  );
--- Device.sql
+++ Device.sql
@@ -2,5 +2,5 @@ DROP TABLE IF EXISTS Device;
 CREATE TABLE Device (
 	ipv4_addr NVARCHAR(40) PRIMARY KEY, -- IPV4 address
         device_name NVARCHAR(40),  -- 機器名稱
-	contract_bandwidth REAL -- 上限值
+	contract_bandwidth INT -- 上限值, 單位 Mbits/sec
 );
