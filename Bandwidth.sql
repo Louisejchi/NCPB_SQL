7a9bc3d611864b40021dfb120227c29dee893a90
louise <a115744446@gmail.com>
2024-03-11 05:43:58 +0800

    Ask not null to some columns

--- Bandwidth.sql
+++ Bandwidth.sql
@@ -1,8 +1,8 @@
 DROP table if exists Bandwidth;
 CREATE TABLE Bandwidth(
-        ipv4_addr NVARCHAR(40), -- IPV4 address
+        ipv4_addr NVARCHAR(40) NOT NULL, -- IPV4 address
         day DATETIME DEFAULT (STRFTIME('%Y-%m-%d', CURRENT_TIMESTAMP, 'localtime')),
         hms DATETIME DEFAULT (STRFTIME('%H:%M:%S', CURRENT_TIMESTAMP, 'localtime')),
-        measured_bandwidth INT, -- 測量結果, Mbits/sec
+        measured_bandwidth INT NOT NULL, -- 測量結果, Mbits/sec
         FOREIGN KEY(ipv4_addr) REFERENCES Device (ipv4_addr)
  );
